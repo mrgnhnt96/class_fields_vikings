a5b73c0ad08961994e2c26c1b7695baa9399cc25
Morgan Hunt <mrgnhnt96@gmail.com>
2022-01-19 13:33:28 -0600

    feat: create build_runner entry point

--- class_fields_generator/lib/class_fields.dart
+++ class_fields_generator/lib/class_fields.dart
@@ -1,3 +1,10 @@
-library class_fields_generator;
+import 'package:build/build.dart';
+import 'package:class_fields/src/class_fields_generator.dart';
+import 'package:source_gen/source_gen.dart';
 
-export 'src/class_fields_generator.dart';
+Builder classFieldsBuilder(BuilderOptions options) {
+  return SharedPartBuilder(
+    [const FieldsGenerator()],
+    'class_fields',
+  );
+}
